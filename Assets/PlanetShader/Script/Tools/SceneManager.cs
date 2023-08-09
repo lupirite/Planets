e09e536e38809dbc26bbd106b1e3ed1cd1c800e8
lupirite <97484881+lupirite@users.noreply.github.com>
2023-08-09 14:25:19 -0600

    BlackHoleSkybox

---
+++ Assets/PlanetShader/Script/Tools/SceneManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour
+{
+
+    public float planetRotSpeed = 1.0f;
+    public float sunRotSpeed = 1.0f;
+    public Transform sun;
+    public Transform[] planets = new Transform[9];
+
+    void Update()
+    {
+        foreach (Transform planet in planets)
+            planet.Rotate(new Vector3(0, planetRotSpeed * Time.deltaTime, 0));
+        if (Input.GetKey(KeyCode.Z))
+            sun.Rotate(new Vector3(0, sunRotSpeed * Time.deltaTime, 0));
+        else if (Input.GetKey(KeyCode.C))
+            sun.Rotate(new Vector3(0, -sunRotSpeed * Time.deltaTime, 0));
+    }
+}
---
+++ Assets/PlanetShader/Script/Tools/ShowOffSceneManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowOffSceneManager : MonoBehaviour {
+	
+	public float		planetRotSpeed = 1.0f;
+	public float		sunRotSpeed = 1.0f;
+	public Transform	sun;
+	public GameObject[]	planets = new GameObject[9];
+
+	private int			index = 0;
+	private GameObject 	planet;
+
+	void Start ()
+	{
+		// spawn the first planet
+		planet = GameObject.Instantiate(planets[0], Vector3.zero, planets[0].transform.rotation) as GameObject;
+	}
+
+	void Update ()
+	{
+		planet.transform.Rotate(new Vector3(0, planetRotSpeed * Time.deltaTime, 0));
+		sun.Rotate(new Vector3(0,sunRotSpeed * Time.deltaTime, 0));
+	}
+
+	public void NextButton()
+	{
+		index += 1;
+		if (index >= planets.Length)
+			index = 0;
+		Destroy(planet);
+		planet = GameObject.Instantiate(planets[index], Vector3.zero, planets[index].transform.rotation) as GameObject;
+		planet.transform.localScale = new Vector3(1,1,1);
+
+	}
+
+	public void PreviousButton()
+	{
+		index -= 1;
+		if (index < 0)
+			index = planets.Length - 1;
+		Destroy(planet);
+		planet = GameObject.Instantiate(planets[index], Vector3.zero, planets[index].transform.rotation) as GameObject;
+		planet.transform.localScale = new Vector3(1,1,1);
+	}
+}
